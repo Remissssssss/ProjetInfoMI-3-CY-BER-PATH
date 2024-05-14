83ee5cd611f5696f49b8e9701c75bafa16097145
kaiiidow <adame.archi@gmail.com>
2024-05-14 20:27:13 +0200

    Update display.c

--- display.c
+++ display.c
@@ -8,27 +8,6 @@
 
 
 
-void consturcteur_murs_(){
-    int a; 
-    int b; 
-    for(int i=0; i<2* hauteur+1;i++){
-        for(int j=0; j<2*largeur+1;j++){
-            if(grille[i][j]>=65 && grille[i][j]<=122){
-                a=rand()%2;
-                b=rand()%2;
-                if(a==0){
-                    a--;
-                }
-                if(b==0){
-                    b--;
-                }
-                grille[i+a][j]=205;
-                grille[i][j+b]=186;
-            }
-        }
-    }
-}
-
 
 
 
