926aaa133755cd42f64407f53cb470ac33b292dd
kaiiidow <adame.archi@gmail.com>
2024-05-14 23:18:51 +0200

    Update jouer_gagnant.c

--- jouer_gagnant.c
+++ jouer_gagnant.c
@@ -4,8 +4,8 @@
 #include "aacontours.h"
 #include "aagrille.h"
 #include "aajouer_coup.h"
-#include "jouer_gagnant.h"
-#include "murs.h"
+#include "aajouer_gagnant.h"
+#include "aamurs.h"
 
 
 void jouer(int* tab_coup, int nb_joueur){
